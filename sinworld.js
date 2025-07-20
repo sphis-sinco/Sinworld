9578cee7983a679b40da8a232a7603fd8e515188
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-19 22:08:30 -0500

    black screen

--- sinworld.js
+++ sinworld.js
@@ -1,1 +1,52 @@
-console.log('Greetings Earth');
+const canvas = document.getElementById("viewport");
+const ctx = canvas.getContext("2d");
+
+var pixelScale = 1;
+
+var width = 640 / pixelScale;
+var height = 480 / pixelScale;
+
+var viewportPixels = [];
+
+const color_black = "black";
+
+function placePixel(x = 0, y = 0, color = "black") {
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, pixelScale, pixelScale);
+}
+
+function initalizeViewportPixels() {
+        let y = 0;
+        while (y < height) {
+                let x = 0;
+                while (x < width) {
+                        viewportPixels.push(color_black);
+
+                        x++;
+                }
+
+                y++;
+        }
+}
+
+console.log(viewportPixels);
+initalizeViewportPixels();
+
+function update() {
+        let x = 0;
+        let y = 0;
+        for (const pixel in viewportPixels) {
+                const color = viewportPixels[pixel];
+
+                placePixel(x, y, color);
+
+                x++;
+                if (x > width)
+                {
+                        x = 0;
+                        y++;
+                }
+        }
+}
+
+setTimeout(update(), 1 / 24);
