10306b7da235f3014e03fcda23c1de980e07846f
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-19 23:05:06 -0500

    + and - seed buttons

--- seedDisplay.js
+++ seedDisplay.js
@@ -10,3 +10,17 @@ updateSeedValueDisplay();
 seedInput.addEventListener("input", () => {
         updateSeedValueDisplay();
 });
+
+const addition = 1;
+
+const minusButton = document.getElementById("minusButton");
+const plusButton = document.getElementById("plusButton");
+minusButton.addEventListener("click", () => {
+        seedInput.value -= addition;
+        updateSeedValueDisplay();
+});
+plusButton.addEventListener("click", () => {
+        const nsiv = parseInt(seedInput.value) + addition;
+        seedInput.value = nsiv;
+        updateSeedValueDisplay();
+});
